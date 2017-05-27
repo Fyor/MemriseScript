0d6fc017003d15286552596f943aec19b6bc8c35
Fyor Klein Gunnewiek <fyorkg@gmail.com>
2017-05-27 21:33:56 +0200

    Update MemriseScript-ArrayScript.js
    Added notes and making it understandible.

--- MemriseScript-ArrayScript.js
+++ MemriseScript-ArrayScript.js
@@ -1,6 +1,15 @@
-//Make Memrise Array
+// This is for making an array for my additional Script.
+// You would have to run this on the page with the list of the words on memrise.com
+
+// The is for collum a
 var someArray = $('.things .thing .col_a.text .text').toArray();
 var memriseArray = [];
 for (var i = 0; i < $(".col_a").size(); i++) {
+	memriseArray.push(someArray[i].innerText);
+}
+// This is for collum b
+var someArray = $('.things .thing .col_btext .text').toArray();
+var memriseArray = [];
+for (var i = 0; i < $(".col_b).size(); i++) {
 	memriseArray.push(someArray[i].innerText);
 }
